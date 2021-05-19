3facd60987f1acaf02cef9322f54a745fd8b6326
KarishmaBothara <bothara.karishma@gmail.com>
2021-05-19 16:44:13 +1200

    Initial rendering of the collection tokens

--- nft-demo/src/App.js
+++ nft-demo/src/App.js
@@ -6,43 +6,58 @@ import * as nft from './nft';
 const registry = new TypeRegistry();
 const url = 'wss://kong2.centrality.me/public/rata/ws';
 
-function App() {
+function NFTCollection(props) {
   const [tokenInfo, setTokenInfo] = useState(undefined);
+  const collectionId = 'centrality_team_1';
+  const {api} = props;
+  useEffect( () => {
+    async function fetch() {
+      if (api) {
+        const tokenInfos = await api.derive.nft.tokenInfoForCollection(collectionId);
+        setTokenInfo(tokenInfos);
+      }
+    }
+    fetch();
+  })
+  return (
+      <div>
+      <h1>Collection, {collectionId}</h1>
+      <table>
+        {tokenInfo?.map(({tokenId, tokenDetails, owner}) => (
+            <tr key={tokenId.toString()}>
+              <td>
+                {tokenId.toString()}
+              </td>
+              <td >
+                {tokenDetails.toString()}
+              </td>
+              <td>
+                {owner.toString()}
+              </td>
+            </tr>
+        ))}
+      </table>
+      </div>
+  );
+}
 
-  useEffect(() => {
-    const derives = { nft };
+function App() {
+  const [api, setApi] = useState(undefined);
+
+  useEffect( () => {
+    const derives = {nft};
     const api = new ApiPromise({provider: url, registry, derives});
     api.on('ready', async () => {
-      console.log('API:::',api);
-      const collectionId = 'kenya_1';
-      const tokenInfos = await api.derive.nft.tokenInfoForCollection(collectionId);
-      tokenInfos.map(({tokenId, tokenDetails, owner}) => {
-        console.log('tokenId:',tokenId.toString());
-        console.log('tokenDetails:', tokenDetails.toString());
-        console.log('Owner:', owner.toString());
-        return tokenId;
-      });
-      setTokenInfo(tokenInfo);
+      setApi(api);
     });
   });
 
+  if (!api) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"https://www.financemagnates.com/wp-content/uploads/2021/03/nft-01.png"} alt="logo" />
-        <p>
-          NFT DEMO
-        </p>
-        <a
-          className="App-link"
-          href="https://www.financemagnates.com/wp-content/uploads/2021/03/nft-01.png"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <NFTCollection api={api}></NFTCollection>
   );
 }
 
