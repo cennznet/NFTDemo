b3efb3b2e618fb703cbc4bad96ef5ca09e7454f0
KarishmaBothara <bothara.karishma@gmail.com>
2021-05-19 14:33:23 +1200

    Added cennznet api dependencies

---
+++ nft-demo/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"https://www.financemagnates.com/wp-content/uploads/2021/03/nft-01.png"} alt="logo" />
+        <p>
+          NFT DEMO
+        </p>
+        <a
+          className="App-link"
+          href="https://www.financemagnates.com/wp-content/uploads/2021/03/nft-01.png"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
